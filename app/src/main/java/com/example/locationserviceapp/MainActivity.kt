d8df2a296994921f5051e9c0871605519fd98042
marsel <ilnar_gumerov_2000@mail.ru>
2021-05-26 22:51:04 +0300

    MainActivity разрешение на геолокацию и запуск сервиса

--- app/src/main/java/com/example/locationserviceapp/MainActivity.kt
+++ app/src/main/java/com/example/locationserviceapp/MainActivity.kt
@@ -1,20 +1,58 @@
 package com.example.locationserviceapp
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private var isRunning: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         startServiceButton.setOnClickListener {
-            startService(Intent(this, LocationService::class.java))
+            if (this.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+                requestLocationPermission()
         }
         stopServiceButton.setOnClickListener {
-            stopService(Intent(this, LocationService::class.java))
+            isRunning = false
+            changeServiceState()
         }
+    }
+
+    private fun requestLocationPermission() {
+        this.requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 0)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 0
+            && permissions.first() == Manifest.permission.ACCESS_FINE_LOCATION
+            && grantResults.first() == PackageManager.PERMISSION_GRANTED
+        ) {
+            isRunning = true
+            changeServiceState()
+        }
+    }
+
+    private fun changeServiceState() {
+        if (isRunning)
+            sendCommand("start")
+        else
+            sendCommand("stop")
+    }
+
+    private fun sendCommand(command: String) {
+        val intent = Intent(this, LocationService::class.java)
+        intent.putExtra("command", command)
+        startService(intent)
     }
 }
